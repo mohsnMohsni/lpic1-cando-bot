43ca07f9a5cac7bb0cf61e7c7f7d6859df2772d1
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-11 13:23:57 +0330

    FIX: add log to check

--- app/decorators.py
+++ app/decorators.py
@@ -7,17 +7,19 @@ from pyrogram.types import Message
 from .constants import messages
 
 
-def command_count_validator(valid_parameter_length: int) -> Callable:
-    def command_count_validator_decorator(func: Callable) -> Callable:
+def command_length_validator(valid_parameter_length: int) -> Callable:
+    print('from level 1')
+    def command_length_validator_decorator(func: Callable) -> Callable:
+        print('from level 2')
         async def wrapper(message: Message, *args, **kwargs) -> None:
+            print('from level 3')
             if len(message.command) != valid_parameter_length:
                 await message.reply(messages.PARAMETERS_NOT_VALID)
                 return
             await func()
-
         return wrapper
 
-    return command_count_validator_decorator
+    return command_length_validator_decorator
 
 
 def command_validator(command_index: int, validator: Callable, err_message: str) -> Callable:
